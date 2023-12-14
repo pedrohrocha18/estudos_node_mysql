b647c1b2059b8f684f143d90998ad24aa9f94cf6
pedrohrocha18 <phr.tech@outlook.com>
2023-12-14 08:45:04 -0300

    Iniciando Projeto

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+
+const app = express();
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("O servidor está conectado.");
+});
