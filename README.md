35a37d80d12f16432e2be83672c4bcd428de6b1c
pedrohrocha18 <phr.tech@outlook.com>
2023-12-15 09:18:34 -0300

    Updade Readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Estudos de Node.Js
+
+Desenvolvi este projeto com a finalidade de consolidar os conhecimentos adquiridos durante meu curso de Node.js. Utilizei não apenas meu domínio em HTML e CSS, mas também as habilidades adquiridas nas tecnologias Node, Express, Express Handlebars e MySQL.
+
+## Pré-requisitos
+
+Certifique-se de ter o Node.js instalado na sua máquina.
+
+## Instalação
+
+1. Clone este repositório: `git clone https://github.com/pedrohrocha18/estudos_node_mysql.git`
+2. Navegue até o diretório do projeto: `cd estudos_node_mysql`
+3. Instale as dependências: `npm i nodemon --save-dev`
+
+## Uso
+
+Para iniciar a aplicação, use o seguinte comando:
+
+```bash
+npm run dev
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
+app.use(express.static("public"));
 app.use(
   express.urlencoded({
     extended: true,
@@ -17,6 +18,39 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+//post
+app.post("/insertbanda", (req, res) => {
+  const nameband = req.body.nameband;
+  const musicband = req.body.musicband;
+  const styleband = req.body.styleband;
+
+  const query = `INSERT INTO bandas (nameband, musicband, styleband) VALUES ('${nameband}','${musicband}','${styleband}') `;
+
+  conexao.query(query, (err) => {
+    if(err) {
+      console.log(err)
+    }
+    res.redirect("/")
+  })
+});
+
+//conexão
+const conexao = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "1807.Drika",
+  database: "db_fixando",
+});
+
+conexao.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Você está conectado ao Banco de Dados =)");
+  }
 });
 
 app.listen(3000, () => {
